e5008964c85f0f336dc18a9d7aa7799dff97f70a
Daksh Devrani <dakshdevrani2789@gmail.com>
2026-07-22 13:40:16 +0530

    fix-try1

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
   ? `${import.meta.env.VITE_API_BASE_URL.replace(/\/+$/, '')}/api/v1`
-  : 'http://192.168.1.226:8000/api/v1'
+  : `http://${window.location.hostname}:8000/api/v1`
 
 export const WS_BASE_URL = import.meta.env.VITE_WS_BASE_URL
   ? `${import.meta.env.VITE_WS_BASE_URL.replace(/\/+$/, '')}/api/v1`
